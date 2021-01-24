a05dbfa85eb6d786b14b3f8cf613c030228f0593
Viesturs Marnauza <v.marnauza@gmail.com>
2021-01-24 14:26:31 +0200

    Add UMD module builds & deploy scripts

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "vue-papercups.js",
+    library: "VuePapercups",
+    libraryTarget: "umd",
+  },
+};
