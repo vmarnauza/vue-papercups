6baa07fd09471d7bf5e61150d2cbb7e5861376d0
Viesturs Marnauza <v.marnauza@gmail.com>
2023-02-16 13:02:27 +0200

    Build for commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "vue-papercups.js",
-    library: "VuePapercups",
-    libraryTarget: "umd",
+    library: {
+      type: "commonjs2",
+    },
   },
 };
